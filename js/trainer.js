bec1753715beb6c8bf478007ef006b1c3196ef9d
sina-ss <sinayaran77@gmail.com>
2023-07-06 14:52:03 +0330

    add responsive style to trainer panel

--- js/trainer.js
+++ js/trainer.js
@@ -1,52 +1,68 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Fetch member data from JSON and populate the attendance table
-    fetch("members.json")
-      .then(response => response.json())
-      .then(data => {
-        const attendanceTableBody = document.querySelector("#attendance-table tbody");
-        data.forEach(member => {
-          const row = document.createElement("tr");
-          row.innerHTML = `
-            <td>${member.name} ${member.familyName}</td>
-            <td>
-              <input type="checkbox" class="attendance-checkbox" data-id="${member.id}" ${member.attendance ? "checked" : ""}>
-            </td>
-          `;
-          attendanceTableBody.appendChild(row);
+// Define the API endpoint
+const API_URL = 'http://localhost:3000';
+
+// Fetch the list of members for the trainer and populate the attendance table with the member data
+async function fetchTrainerMembers(trainerId) {
+    try {
+        const response = await fetch(`${API_URL}/users/trainer-members/${trainerId}`);
+        const data = await response.json();
+
+        const attendanceTable = document.getElementById('attendance-table').getElementsByTagName('tbody')[0];
+        attendanceTable.innerHTML = '';
+
+        data.forEach((member) => {
+            const row = attendanceTable.insertRow(-1);
+            const usernameCell = row.insertCell(0);
+            const nameCell = row.insertCell(1);
+            const familyNameCell = row.insertCell(2);
+            const shiftCell = row.insertCell(3);
+            const attendanceCell = row.insertCell(4);
+
+            usernameCell.innerHTML = member.member.username;
+            nameCell.innerHTML = member.member.name;
+            familyNameCell.innerHTML = member.member.familyName;
+            shiftCell.innerHTML = member.member.shift;
+
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.value = member.member.ID;
+            attendanceCell.appendChild(checkbox);
         });
-  
-        // Attach event listeners to attendance checkboxes
-        const attendanceCheckboxes = document.querySelectorAll(".attendance-checkbox");
-        attendanceCheckboxes.forEach(checkbox => {
-          checkbox.addEventListener("change", updateAttendance);
-        });
-      });
-  
-    // Login form submission handler
-    document.getElementById("login-form").addEventListener("submit", function(event) {
-      event.preventDefault();
-  
-      // Get the entered username and password
-      const username = document.getElementById("username").value;
-      const password = document.getElementById("password").value;
-  
-      // Perform login authentication
-      if (username === "trainer" && password === "password") {
-        alert("Login successful!");
-        // Implement redirection or other actions here
-      } else {
-        alert("Invalid username or password. Please try again.");
-      }
-    });
-  
-    // Update attendance status on checkbox change
-    function updateAttendance(event) {
-      const memberId = event.target.getAttribute("data-id");
-      const isChecked = event.target.checked;
-  
-      // Update the attendance status in the JSON file or make an API call here
-      console.log(`Member ID: ${memberId}, Attendance: ${isChecked}`);
-      // ...
+    } catch (error) {
+        console.error(error);
     }
-  });
-  
+}
+
+// Update the attendance status of each member in the database when the trainer marks the attendance using the checkboxes
+async function updateAttendanceStatus(trainerId, memberId, isChecked) {
+    try {
+        const response = await fetch(`${API_URL}/attendance/update`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                trainerId,
+                memberId,
+                isChecked,
+            }),
+        });
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Attach an event listener to the attendance table to update the attendance status when the trainer marks the attendance using the checkboxes
+const attendanceTable = document.getElementById('attendance-table');
+attendanceTable.addEventListener('change', (event) => {
+    const trainerId = 1; // Change this to the ID of the logged-in trainer
+    const memberId = event.target.value;
+    const isChecked = event.target.checked;
+
+    updateAttendanceStatus(trainerId, memberId, isChecked);
+});
+
+// Fetch the list of members for the trainer and populate the attendance table on page load
+fetchTrainerMembers(1); // Change this to the ID of the logged-in trainer
